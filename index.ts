3087f81e05df758079db7522adbe5713db2304e3
madhav-k <madhavkarthickk.1212@gmail.com>
2025-03-05 23:52:49 +0530

    block two

--- index.ts
+++ index.ts
@@ -32,3 +32,37 @@ const main = async () => {
 };
 
 main();
+import {  Client,  Wallet }  from "xrpl" 
+
+const client = new Client("wss://s.altnet.rippletest.net:51233")
+
+
+const main = async () => {
+  console.log("lets get started...");
+  await client.connect();
+
+  // do something interesting here
+
+
+  await client.disconnect();
+  console.log("all done!");
+};
+
+main();
+import {  Client,  Wallet }  from "xrpl" 
+
+const client = new Client("wss://s.altnet.rippletest.net:51233")
+
+
+const main = async () => {
+  console.log("lets get started...");
+  await client.connect();
+
+  // do something interesting here
+
+
+  await client.disconnect();
+  console.log("all done!");
+};
+
+main();
